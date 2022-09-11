3c15a6410afee0902ace5d79b82ec629db264c79
Aleksandr Serikov <Sashas840@gmail.com>
2022-09-11 11:26:47 +0300

    ESLint error fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,6 @@ export default function Home() {
   ];
   const apiURL =
     "https://6307af893a2114bac76922d9.mockapi.io/photos/react-pizza";
-  // let sortType = "asc";
 
   // Hooks
   const [pizzas, setPizzas] = React.useState([]);
@@ -24,22 +23,26 @@ export default function Home() {
   const [sortTypeId, setSortTypeId] = React.useState(0);
   const [isSortTypeAsc, setIsSortTypeAsc] = React.useState(true);
 
-  React.useEffect(() => {
-    window.scrollTo(0, 0);
-    setIsLoading(true);
+  React.useEffect(
+    () => {
+      window.scrollTo(0, 0);
+      setIsLoading(true);
 
-    const category = categoryId !== 0 ? `category=${categoryId}&` : "";
-    const sortType = isSortTypeAsc ? "asc" : "desc";
-    const sort = `sortBy=${sortOptions[sortTypeId].sortParam}&order=${sortType}`;
-    const fetchParams = "?" + category + sort;
+      const category = categoryId !== 0 ? `category=${categoryId}&` : "";
+      const sortType = isSortTypeAsc ? "asc" : "desc";
+      const sort = `sortBy=${sortOptions[sortTypeId].sortParam}&order=${sortType}`;
+      const fetchParams = "?" + category + sort;
 
-    fetch(apiURL + fetchParams)
-      .then((response) => response.json())
-      .then((responseData) => {
-        setPizzas(responseData);
-        setIsLoading(false);
-      });
-  }, [categoryId, sortTypeId, isSortTypeAsc]);
+      fetch(apiURL + fetchParams)
+        .then((response) => response.json())
+        .then((responseData) => {
+          setPizzas(responseData);
+          setIsLoading(false);
+        });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [categoryId, sortTypeId, isSortTypeAsc]
+  );
 
   return (
     <>
