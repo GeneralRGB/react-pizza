9b7d6d16a978eb9812ddd62fdfa43c91c9f7a576
Aleksandr Serikov <Sashas840@gmail.com>
2022-11-15 21:53:50 +0300

    React bundle optimizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 // Modules
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 // Styles
-import "./scss/app.scss";
+import './scss/app.scss';
 
 // Components
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Cart from "./pages/Cart";
-import PizzaInfo from "./pages/PizzaInfo";
-import MainLayout from "./layouts/MainLayout";
+import Home from './pages/Home';
+// import NotFound from './pages/NotFound';
+// import Cart from "./pages/Cart";
+// import PizzaInfo from './pages/PizzaInfo';
+import MainLayout from './layouts/MainLayout';
+
+// Lazy loading configuration
+const Cart = React.lazy(
+	() => import(/* webpackChunkName: "[Cart]" */ './pages/Cart')
+);
+const PizzaInfo = React.lazy(
+	() => import(/* webpackChunkName: "[Pizza Info]" */ './pages/PizzaInfo')
+);
+const NotFound = React.lazy(
+	() => import(/* webpackChunkName: "[Not Found]" */ './pages/NotFound')
+);
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:id" element={<PizzaInfo />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<MainLayout />}>
+				<Route path="" element={<Home />} />
+				<Route
+					path="cart"
+					element={
+						<Suspense fallback={<h2>Loading...</h2>}>
+							<Cart />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="pizza/:id"
+					element={
+						<Suspense fallback={<h2>Loading...</h2>}>
+							<PizzaInfo />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="*"
+					element={
+						<Suspense fallback={<h2>Loading...</h2>}>
+							<NotFound />
+						</Suspense>
+					}
+				/>
+			</Route>
+		</Routes>
+	);
 }
--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactPaginate from 'react-paginate';
 import { useSelector } from 'react-redux';
-import { selectCurrentPage } from '../../redux/slices/filter/selectors';
 
 import { setCurrentPage } from '../../redux/slices/filter/slice';
 import { RootState, useAppDispatch } from '../../redux/store';
@@ -11,7 +10,6 @@ export default function Pagination() {
 	const dispatch = useAppDispatch();
 
 	const { pagesAmount } = useSelector((state: RootState) => state.pizzaSlice);
-	const currentPage = useSelector(selectCurrentPage);
 
 	return (
 		<ReactPaginate
@@ -22,8 +20,6 @@ export default function Pagination() {
 			onPageChange={(event) => dispatch(setCurrentPage(event.selected + 1))}
 			pageRangeDisplayed={4}
 			pageCount={pagesAmount}
-			initialPage={currentPage}
-			renderOnZeroPageCount={() => null}
 		/>
 	);
 }
